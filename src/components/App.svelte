a4118098699a8a37159502e2332e2dcff18776d1
yizhe-ang <ang.yizhe@u.nus.edu>
2023-07-14 11:30:07 +0800

    only run intro animation on first visit

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -6,8 +6,13 @@
 	import Works from "$components/Works.svelte";
 	import Footer from "$components/Footer.svelte";
 	import Spacer from "$components/Spacer.svelte";
+	import { numVisits } from "$stores/localStorage.js";
 	import data from "$data/works.csv";
 	import { group } from "d3";
+
+	// Update number of user visits
+	$numVisits += 1;
+	$: console.log($numVisits);
 
 	// FIXME: Can this be done elsewhere for optimization?
 	// Process data
@@ -24,24 +29,26 @@
 
 		const tl = gsap.timeline();
 
-		tl.from("body", { autoAlpha: 0 })
-			.from("h1", { opacity: 0, duration: 1 })
-			//
-			.add("a", "-=0.5")
-			.from("#visual", { opacity: 0, duration: textDuration }, "a")
-			.from("#interactive", { opacity: 0, duration: textDuration }, "a+=0.05")
-			//
-			.add("b")
-			.from("#stories", { opacity: 0, duration: textDuration }, "b")
-			.from("#explorables", { opacity: 0, duration: textDuration }, "b+=0.05")
-			//
-			.add("content")
-			.from("#socials", { opacity: 0 }, "content")
-			.from("#grid-pattern", { opacity: 0 }, "content")
-			.from("#gradient", { opacity: 0 }, "content")
-			.from(".works", { opacity: 0 }, "content");
-		// .from("#body-texture", { opacity: 0 }, "content")
-		// .from("footer", { opacity: 0 }, "content")
+		if ($numVisits > 1) {
+			tl.from("body", { autoAlpha: 0 }).add("content");
+		} else {
+			tl.from("body", { autoAlpha: 0 })
+				.from("h1", { opacity: 0, duration: 1 })
+				//
+				.add("a", "-=0.5")
+				.from("#visual", { opacity: 0, duration: textDuration }, "a")
+				.from("#interactive", { opacity: 0, duration: textDuration }, "a+=0.05")
+				//
+				.add("b")
+				.from("#stories", { opacity: 0, duration: textDuration }, "b")
+				.from("#explorables", { opacity: 0, duration: textDuration }, "b+=0.05")
+				//
+				.add("content")
+				.from("#socials", { opacity: 0 }, "content")
+				.from("#grid-pattern", { opacity: 0 }, "content")
+				.from("#gradient", { opacity: 0 }, "content")
+				.from(".works", { opacity: 0 }, "content");
+		}
 
 		const types = ["portfolio", "sketches"];
 		types.forEach((type) => {
